c9f2ecb82f644c2200fb446b8e97d6370b3333e5
Abhishek-Kuvalekar <abhi.kuvalekar@gmail.com>
2018-01-21 19:15:29 +0530

    Created basic website to enable downloading of papers.

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from flask import render_template
+from app import app
+import os
+
+@app.route('/')
+def homepage():
+    'Renders home.html.'
+    return render_template('home.html')
+
+@app.route('/branch/<branchName>')
+def showContent(branchName = None):
+    'Creates a dictionary of contents of each branch directory.'
+    
+    if(branchName == None):
+        return render_template('home.html')
+    
+    path = 'app/static/papers/' + branchName
+    contentDict = dict()
+    
+    for year in os.listdir(path):
+        if(not os.path.isdir(path + '/' + year)):
+            continue
+        contentDict[year] = dict()
+        for sub in os.listdir(path + '/' + year):
+            if(not os.path.isdir(path + '/' + year + '/' + sub)):
+                continue
+                
+            contentDict[year][sub] = os.listdir(path + '/' + year + '/' + sub)
+        
+    contentDict['branchName'] = branchName
+    return render_template('content.html', contentDict = contentDict, header_title = branchName)
